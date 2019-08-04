2b2a86ffe9a655f95c7b1c5fae9f7a0f5420350e
Reda Lemeden <git@kaishin.haz.email>
2019-08-04 13:25:41 +0200

    Add sample data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'SwiftUI Directory',
+    description: 'A curated directory of SwiftUI open-source packages.',
+    author: 'Reda Lemeden'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-yaml',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/data/`
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
     },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'swiftui-directory',
+        short_name: 'SwiftUI Directory',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    // 'gatsby-plugin-offline',
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <h1>SwiftUI Directory</h1>
 
-export default IndexPage
+    <ol className="package-list">
+      {data.allPackages.edges.map(({ node }, id) => (
+        <li className="package">
+          {node.name} <small>{node.description}</small>
+        </li>
+      ))}
+    </ol>
+  </Layout>
+);
+
+export const query = graphql`
+  query PackagesQuery {
+    allPackages: allPackagesYaml {
+      edges {
+        node {
+          name
+          description
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
