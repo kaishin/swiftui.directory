d899a68ae19dca4371b96de4e4cb2e64428c19dc
Reda Lemeden <git@redalemeden.com>
2019-09-16 23:56:17 +0200

    Remove unused plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,6 @@ module.exports = {
       }
     },
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`
-      }
-    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
